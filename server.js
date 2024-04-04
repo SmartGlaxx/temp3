98adcff95351b7bbc6b09f82a189d6afa289b6c1
smartglaxx <smartegbuchulem@gmail.com>
2024-04-03 19:53:46 -0700

    another

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const app = express()
 
+const productRouter = require("./routes/productRoutes")
+
+app.use("/products", productRouter)
 app.get("/",(req,res)=>{
-    res.send("working edited further")
+    res.send("working edited again")
 })
+
 
 app.listen(5001, ()=>{
     console.log("listening on 5001")
